aca9711f0ed577150f97749c0dcc9606ad9d8436
Nicolas Perez <nicodavidperezescobar@gmail.com>
2026-04-08 19:51:01 -0500

    Add change password admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,31 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 from .models import User
+
+@admin.action(description="Cambiar contraseña a los usuarios seleccionados")
+def update_passwords_action(modeladmin, request, queryset):
+    if request.POST.get('apply'):
+        new_pass = request.POST.get('new_password')
+        if new_pass:
+            for user in queryset:
+                user.set_password(new_pass)
+                user.save()
+            modeladmin.message_user(request, f"Se actualizó la contraseña de {queryset.count()} usuario(s) exitosamente.")
+            return HttpResponseRedirect(request.get_full_path())
+        else:
+            modeladmin.message_user(request, "La contraseña no puede estar vacía.", level=messages.ERROR)
+            return HttpResponseRedirect(request.get_full_path())
+
+    context = {
+        'queryset': queryset,
+        'action_checkbox_name': admin.helpers.ACTION_CHECKBOX_NAME,
+        'title': 'Designe nueva contraseña',
+    }
+    return render(request, 'admin/change_password_action.html', context)
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
@@ -18,3 +42,5 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (
         ('S-21-S Custom Fields', {'fields': ('role', 'congregation')}),
     )
+
+    actions = [update_passwords_action]
